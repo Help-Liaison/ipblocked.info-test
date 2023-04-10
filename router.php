8b9d30c932d902d3d804da5476b056f0424c06de
John Hurley <john@helpliaison.com>
2023-04-10 18:44:28 -0400

    Update router.php
    implemented start of routing functionality.

--- router.php
+++ router.php
@@ -2,6 +2,29 @@
     // Page Router
     // This file essentially routes requests to the right pages 
 
-    
+    global $what;
+    global $what2;
 
+    $what = filter_var($_GET['what'],FILTER_SANITIZE_URL);
+    $what2 = filter_var($_GET['what2'],FILTER_SANITIZE_URL);
+
+    if (!$what)     die('Primary Task not specified.');
+    if (!$what2)    die('Secondary Task not specified.');
+
+    $pages = scandir($paths['pages']);
+    $page_requested = $what . '.php';
+    $page_file = $paths['pages'] . $page_requested;
+
+    if ($page_requested == $pages)
+    {
+        // Check if Page Exists and then Load it if it does
+        require($page_file);
+    }
+
+    /*
+    $files_modules = glob($paths['pages'] . '*.php');
+    foreach ($files_modules as $file_module) {
+        require_once($file_module);
+    }
+    */
 ?>
