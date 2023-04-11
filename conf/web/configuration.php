bbb0cc43eb067693fe60fd73ada61765ef857bad
John Hurley <john@helpliaison.com>
2023-04-11 09:03:51 -0400

    re-structure layout
    cleaned up directory structure

---
+++ conf/web/configuration.php
@@ -0,0 +1,17 @@
+<?php
+    // IPBlocked Configuration File
+
+    global $config;
+    global $paths;
+
+    $config = array(
+
+    );
+
+    $paths = array(
+        'root'      =>  __DIR__ .'/',
+        'includes'  =>  'includes'.'/',
+        'modules'   =>  'modules'.'/',
+    );
+
+?>
